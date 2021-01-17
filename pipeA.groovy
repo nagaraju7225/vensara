d7fe72b3ff3d9eafe2e02d2c2481ba6329dccc62
nagaraju7225 <34837887+nagaraju7225@users.noreply.github.com>
2021-01-17 18:31:59 +0530

    Update pipeA.groovy
    add

--- pipeA.groovy
+++ pipeA.groovy
@@ -1,8 +1,8 @@
 @Library('project-libraries') _
 verifytoJS{
   Branch = 'master'
-  ScmUrl = 'https://github.com/kyash55/Ansible.git'
-  Credentials = '6a1f806b-f550-469e-8a0d-05e60efc3033'
+  ScmUrl = 'https://github.com/nagaraju7225/myfirstrepo.git'
+  Credentials = 'git_cred'
   }
 
     
